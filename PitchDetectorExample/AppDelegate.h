089294304a5b015c9e88a68252cc65d976fd2c13
Kevin Murphy <kpmurphy91@gmail.com>
2012-07-04 20:06:00 -0400

    Initial Commit

---
+++ PitchDetectorExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PitchDetectorExample
+//
+//  Created by Kevin Murphy on 7/4/12.
+//  Copyright (c) 2012 New York University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PitchDetectorExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PitchDetectorExample
+//
+//  Created by Kevin Murphy on 7/4/12.
+//  Copyright (c) 2012 New York University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
